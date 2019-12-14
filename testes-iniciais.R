700ace8f081025db47f183b6055cfc82a9eeedc6
neuremberg <neur.matos@gmail.com>
2019-12-14 18:10:50 -0200

    Adição de novo método de otimização

--- testes-iniciais.R
+++ testes-iniciais.R
@@ -1,15 +1,7 @@
-
-# Simulando a variância de carteiros com n pesos iguais
+# == Implementação à mão == #
 
 sd=0.50; cv=0.05; m=100
 sd_p = matrix(0,m,1) # Alocando memória para receber os pesos
-
-for (j in 1:m) {
-  cv_mat = matrix(1,j,j)*cv # Inicializando matriz de covariância
-  diag(cv_mat) = sd^2       # Modificando diagonal da matriz
-  w = matrix(1/j,j,1)       # Definindo pesos das ações na carteira
-  sd_p[j] = sqrt(t(w) %*% cv_mat %*% w) # Calculando risco das carteiras
-}
 
 
 simular_carteira <- function(n, sd, cv){
@@ -18,7 +10,7 @@ simular_carteira <- function(n, sd, cv){
   cv_mat = matrix(1, n, n)*cv
   diag(cv_mat) = sd^2
   
-  # Pesos
+  # Calculando os pesos
   w = matrix(1/n, n, 1)
   
   # Variância da carteira
@@ -27,10 +19,34 @@ simular_carteira <- function(n, sd, cv){
   return(sd_p[1,1])
 }
 
-
+min(retornos_esperados)
+max(retornos_esperados)
 saida <- vapply(1:100, FUN = simular_carteira, sd = sd, cv = cv, FUN.VALUE = double(1))
 
 simular_carteira(4, sd = sd, cv = cv)
 
 plot(sd_p,type="l",col="blue")
 
+
+markowitz <-  function(mu,cv,Er) {
+  n = length(mu)
+  wuns = matrix(1,n,1)
+  A = t(wuns) %*% solve(cv) %*% mu
+  B = t(mu) %*% solve(cv) %*% mu
+  C = t(wuns) %*% solve(cv) %*% wuns
+  D = B*C - A^2
+  lam = (C*Er-A)/D
+  gam = (B-A*Er)/D
+  wts = lam[1]*(solve(cv) %*% mu) + gam[1]*(solve(cv) %*% wuns)
+  g = (B[1]*(solve(cv) %*% wuns) - A[1]*(solve(cv) %*% mu))/D[1]
+  h = (C[1]*(solve(cv) %*% mu) - A[1]*(solve(cv) %*% wuns))/D[1]
+  wts = g + h*Er
+  
+  return(wts)
+}
+
+markowitz(
+  mu = retornos_esperados,
+  cv = matriz_cova,
+  Er = .001
+)
